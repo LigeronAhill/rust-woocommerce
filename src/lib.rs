1802e5e7f1946b34a8da5164e43ad8c2569fd88d
Aleksandr <provotorov@inbox.ru>
2024-04-11 17:05:01 +0300

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,8 @@ mod models;
 pub use self::controllers::ApiClient;
 pub use models::{
     coupons::*, customers::*, data::*, orders::*, product_attributes::*, product_categories::*,
-    product_reviews::*, product_variations::*, products::*, reports::*, system_status_tools::*,
-    webhooks::*, BatchObject, MetaData,
+    product_reviews::*, product_variations::*, products::*, reports::*, webhooks::*, BatchObject,
+    MetaData,
 };
 mod config;
 pub use config::Config;
